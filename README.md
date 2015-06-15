e7f32cbc48a4e84a568577c2d97b58068444a3f7
michelve <michelve@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-15 19:50:33 +0000

    release version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/page-switcher/tags/1.0.1@1181347 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.md
@@ -0,0 +1,31 @@
+=== Page Switcher ===
+Tags: page switcher, post switcher, switch, change page, smart search, switch pages
+Requires at least: 3.0.0
+Tested up to: 4.2.2
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Easily change or switch the current page to another pages from the wordpress editor.
+
+== Description ==
+Easily change or switch the current page to another pages from the wordpress editor. It also adds a search widget to the dashboard to access your pages faster, also there is a added filter to post types as well. Also the main search box inside the page edot was replace with this plugin
+
+== Installation ==
+Install the plugin into the plugins/page-switcher directory, and activate.
+From the post edit screen, above the \"Publish\" button is the \"Post Type\" interface.
+Change post types as needed.
+
+== Screenshots ==
+
+1. This is the first screen shot
+'/assets/screenshot-1.png'
+
+2. This is the second screen shot
+'/assets/screenshot-2.png'
+
+3. This is the third screen shot
+'/assets/screenshot-3.png'
+
+4. This is the fouth screen shot
+'/assets/screenshot-4.png'
---
+++ page-switcher.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name:       Page Switcher
+ * Plugin URI:        http://velismichel.com/Page-Switcher.zip
+ * Description:       Easily change or switch the current page to other pages from the wordpress page editor.
+ * Version:           1.0.1
+ * Author:            Michel Velis
+ * Author URI:        http://velismichel.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       page-switcher
+ */
+
+define('PageSwitcher_CON', TRUE);
+
+function ps_admin_styles() {
+    wp_enqueue_style('select2-css', plugins_url('assets/css/select2.min.css', __FILE__));
+    wp_enqueue_style('switcher-css', plugins_url('assets/css/page-switcher.css', __FILE__));
+    wp_enqueue_script('select2-js', plugins_url('page-switcher/assets/js/select2.min.js', true, true, true));
+}
+
+
+include( plugin_dir_path( __FILE__ ) . 'admin/widgets.php');
+include( plugin_dir_path( __FILE__ ) . 'admin/get_pages.php');
+include( plugin_dir_path( __FILE__ ) . 'admin/get_post.php');
+
+
+add_action('admin_enqueue_scripts', 'ps_admin_styles');
+add_action( 'add_meta_boxes', 'page_switcher_func' );
+add_action( 'add_meta_boxes', 'post_switcher_func' );
+
